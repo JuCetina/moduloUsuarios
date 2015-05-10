2135c33f26d6d68ae9296b9ba7422630e2a4b5f3
Juliette Cetina <juliethcetina@gmail.com>
2015-05-10 00:20:14 -0500

    Se elimino la carpeta less de resources, gulpfile.js, se integraron componentes HTML y FORM y se utilizaron para modificar el formulario de login.blade.php

--- config/app.php
+++ config/app.php
@@ -145,6 +145,8 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
+		'Collective\Html\HtmlServiceProvider',
+
 	],
 
 	/*
@@ -192,6 +194,9 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+
+		'Form' => 'Collective\Html\FormFacade',
+      	'Html' => 'Collective\Html\HtmlFacade',
 
 	],
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,14 +24,14 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+								{!! Form::text('email', null, ['class' => 'form-control', 'type' => 'email']) !!}
 							</div>
 						</div>
 
 						<div class="form-group">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+								{!! Form::password('password', ['class' => 'form-control']) !!}
 							</div>
 						</div>
 
@@ -39,7 +39,7 @@
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
-										<input type="checkbox" name="remember"> Remember Me
+										{!! Form::checkbox('remember') !!} Remember Me
 									</label>
 								</div>
 							</div>
@@ -47,8 +47,8 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
+								{!! Form::submit('Login', ['class' => 'btn btn-primary']) !!}
+	
 								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
 							</div>
 						</div>
