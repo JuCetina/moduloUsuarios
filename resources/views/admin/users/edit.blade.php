5c0a4337716271f1047377fd2e955c4b97e76b6c
Juliette Cetina <juliethcetina@gmail.com>
2015-05-13 17:53:38 -0500

    Se agrego en la vista para editar el usuario el nombre completo del mismo de manera informativa

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Actualizar usuario: {{ $user->first_name }}</div>
+				<div class="panel-heading">Actualizar usuario: {{ $user->full_name }}</div>
 				<div class="panel-body">
 					@include('admin.partials.messages')
 					
