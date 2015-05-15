9f2ce11eabc7e3195da65153af3b6eb97ce46c8e
Juliette Cetina <juliethcetina@gmail.com>
2015-05-14 19:23:57 -0500

    Se agrego el middleware auth para que los usuarios que iniciaron sesion puedan ingresar al index

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -12,6 +12,10 @@ use Illuminate\SUpport\Facades\Session;
 
 class UsersController extends Controller {
 
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 	/**
 	 * Display a listing of the resource.
 	 *
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::controllers([
 	'password' => 'Auth\PasswordController',
 ]);
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth', 'namespace' => 'Admin'], function() {
 	Route::resource('users', 'UsersController');
 });
 
